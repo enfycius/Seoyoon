60e70f237d89831352a84681b9a56fae4b3ae754
enfycius <hexk0131@gmail.com>
2023-04-13 21:11:15 +0900

    23.04.13

---
+++ Python/ex1-3.py
@@ -0,0 +1,27 @@
+a = input()
+b = input()
+
+
+
+# Integer: dot (x)
+
+# Float: dot (o)
+
+# 10.111
+# 20.12
+
+# 30.123
+
+# 30
+
+
+# print(int(a) + int(b))
+
+# String
+
+# "Hello" + "World"
+
+# HelloWorld
+
+print(str(int(a)) + b)
+
